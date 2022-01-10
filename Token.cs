a81af90fecee44a0b300de5b71db27ac88ce3781
GenerallyStevie <39565821+GenerallyStevie@users.noreply.github.com>
2022-01-09 21:04:54 -0500

    Update Token.cs

--- Token.cs
+++ Token.cs
@@ -1,4 +1,4 @@
 public class Token
 {
-    public static string token = "OTI5OTE1OTY3OTczMjQwODkz.YduRuw.ENjMYEM5pP2hspZlS1GTwca6eVc"; //ENTER TOKEN HERE
+    public static string token = ""; //ENTER TOKEN HERE
 }
